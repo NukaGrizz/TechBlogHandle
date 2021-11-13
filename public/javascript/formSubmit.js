142a607cc1d16a0adb1f19a060feceb54e8393ed
NukaGrizz <nukagrizz@gmail.com>
2021-11-13 00:16:26 -0500

    create crud methods in javascript file form submit for post and import methods to main layout and form partial

---
+++ public/javascript/formSubmit.js
@@ -0,0 +1,125 @@
+function submitCreatePost(e) {
+    e.preventDefault();
+
+    fetch(`/post/create-post`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title: e.target["title"].value,
+            content: e.target["content"].value,
+        })
+    }).then(response => {
+        window.location.href = '/post/dashboard';
+    })
+}
+
+function submitEditPost(e) {
+    e.preventDefault();
+
+    const id = location.pathname.split("/")
+
+    fetch(`/post/${id[2]}/edit`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title: e.target["title"].value,
+            content: e.target["content"].value,
+            // todo: get user id from session
+            user_id: 2,
+        })
+
+    }).then(response => {
+        window.location.href = '/post/dashboard';
+    })
+}
+
+function submitDeletePost(e) {
+    e.preventDefault();
+
+    const id = location.pathname.split("/")
+    console.log(id)
+    fetch(`/post/${id[2]}/edit`, {
+        method: 'DELETE'
+    }).then(response => {
+        window.location.href = '/post/dashboard';
+    })
+}
+
+function submitLogin(e) {
+    e.preventDefault();
+
+    fetch(`/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: e.target["username"].value.trim(),
+            password: e.target["password"].value.trim(),
+        })
+
+    }).then(response => {
+        console.log(response)
+        if (response.status === 200) {
+            window.location.href = '/post/dashboard';
+        }else {
+          response.json()
+              .then (response => {
+                alert(response.message)
+            });
+
+    }})
+}
+
+function submitSignUp(e) {
+    e.preventDefault();
+
+    fetch(`/sign-up`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: e.target["username"].value,
+            password: e.target["password"].value,
+        })
+
+    }).then(response => {
+        console.log(response)
+        if (response.status === 200) {
+            window.location.href = '/post/dashboard';
+        }
+    })
+}
+
+function retrieveAllComments() {
+    e.preventDefault();
+}
+
+async function submitNewComment(e) {
+    e.preventDefault();
+
+    const postId = location.pathname.split("/")
+
+        const response = await fetch(`/post/${postId[2]}/comments`, {
+            method: 'POST',
+            body: JSON.stringify({
+                comment_text: e.target["comment"].value,
+                post_id: postId[2],
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            console.log(response)
+            alert(response.statusText);
+        }
+}
